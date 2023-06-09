4d3423c6e9fe0019f9a23c6ac417b554e811ef7c
petekostrytsya <petekostrytsya@gmail.com>
2023-06-09 20:23:15 +0300

    fix app_1

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 import { ButtonList, Button } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (
-        <ButtonList>
-        {options.map((option) => (
-          <Button key={option} onClick={() => onLeaveFeedback({ option })}>
-            {option}
-          </Button>
-        ))}
-      </ButtonList>
-    )
+  return (
+    <ButtonList>
+      {options.map((option) => (
+        <Button key={option} onClick={() => onLeaveFeedback({ option })}>
+          {option}
+        </Button>
+      ))}
+    </ButtonList>
+  );
 }
 
 FeedbackOptions.propTypes = {
