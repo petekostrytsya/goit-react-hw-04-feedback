8c0c6e1cf83eca5f63f8781d58ee01824c7a7f29
petekostrytsya <petekostrytsya@gmail.com>
2023-06-09 20:24:43 +0300

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
-import { Statistics } from "./Statistics/Statistics";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Section } from "./Section/Section";
-import { NotFeedback } from "./NotFeedback/NotFeedback";
-
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { NotFeedback } from './NotFeedback/NotFeedback';
 
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const onLeaveFeedback = ({ target: { option } }) => {
+  const onLeaveFeedback = ({ option }) => {
     switch (option) {
       case 'good':
         setGood(prevGood => prevGood + 1);
@@ -25,7 +24,7 @@ export const App = () => {
         console.log(`Type feedback option - ${option} is not`);
     }
   };
-  
+
   const countTotalFeedback = obj => {
     return Object.values(obj).reduce((total, curr) => (total += curr));
   };
@@ -34,7 +33,7 @@ export const App = () => {
     if (totalFeedback > 0) return Math.round((good / totalFeedback) * 100);
     return 0;
   };
-  
+
   const totalFeedback = countTotalFeedback({ good, neutral, bad });
   const positivePercentage = countPositiveFeedbackPercentage(totalFeedback);
   return (
